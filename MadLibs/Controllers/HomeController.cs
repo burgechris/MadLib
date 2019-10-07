1076cf06d78e29fe2da7b9838a31930996c2dab0
Mike McShane and sofia giordano <sofia@>
2019-10-07 11:25:00 -0700

    proj done

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route ("/")] 
+        // We use a single slash to create a default page when you loaf the localhost.
+        public ActionResult Form() { return View(); }
+
+
+        [Route ("/output")]
+        public ActionResult Output(string adjectiveOne, string adjectiveTwo, string adjectiveThree, string nounOne, string nounTwo, string nounThree, string nounFour, string properNoun) 
+        {
+
+        MadLibVariable myMadLib = new MadLibVariable();
+        myMadLib.AdjectiveOne = adjectiveOne;
+        myMadLib.AdjectiveTwo = adjectiveTwo;
+        myMadLib.AdjectiveThree = adjectiveThree;
+        myMadLib.NounOne = nounOne;
+        myMadLib.NounTwo = nounTwo;
+        myMadLib.NounThree = nounThree;
+        myMadLib.NounFour = nounFour;
+        myMadLib.ProperNoun = properNoun;
+
+        return View(myMadLib);
+        }
+    }
+}
---
+++ MadLibs/Models/MadLibs.cs
@@ -0,0 +1,14 @@
+namespace MadLibs.Models
+{
+    public class MadLibVariable
+    {
+        public string AdjectiveOne { get; set; }
+        public string AdjectiveTwo { get; set; }
+        public string AdjectiveThree { get; set; }
+        public string NounOne { get; set; }
+        public string NounTwo { get; set; }
+        public string NounThree { get; set; }
+        public string NounFour { get; set; }
+        public string ProperNoun { get; set; }
+    }
+}
